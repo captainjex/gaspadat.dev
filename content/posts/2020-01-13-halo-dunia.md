44cf1c50c228777990b8332333fd6a36be904359
wkwksama <mailpakgasing@gmail.com>
2020-01-13 02:20:08 +0700

    init: after clean template

---
+++ content/posts/2020-01-13-halo-dunia.md
@@ -0,0 +1,15 @@
+---
+title: "Halo Dunia"
+slug: ""
+description: ""
+date: 2020-01-13 01:54:28
+author: "gaspadat"
+tags:
+  - getting-started
+cover: https://avatars3.githubusercontent.com/u/21119252?v=4
+fullscreen: false
+---
+
+baru mencoba menulis post baru
+
+update yaa
---
+++ gridsome.config.js
@@ -0,0 +1,120 @@
+class TailwindExtractor {
+  static extract(content) {
+    return content.match(/[A-Za-z0-9-_:\/]+/g) || [];
+  }
+}
+
+module.exports = {
+  siteName: 'gaspadat site',
+  siteDescription: "This is my site",
+  siteUrl: 'https://gaspadat.netlify.com',
+  titleTemplate: `%s | Forever awesome`,
+  icon: 'src/favicon.png',
+
+  transformers: {
+    remark: {
+      externalLinksTarget: '_blank',
+      externalLinksRel: ['nofollow', 'noopener', 'noreferrer'],
+      plugins: [
+        ['gridsome-plugin-remark-shiki', {
+          theme: 'min-light'
+        }]
+      ]
+    }
+  },
+
+  plugins: [
+    {
+      use: '@gridsome/source-filesystem',
+      options: {
+        path: 'content/posts/**/*.md',
+        typeName: 'Post',
+        refs: {
+          tags: {
+            typeName: 'Tag',
+            create: true,
+          },
+          author: {
+            typeName: 'Author',
+            create: true,
+          },
+        },
+      },
+    },
+    {
+      use: '@gridsome/plugin-google-analytics',
+      options: {
+        id: process.env.G_ANALYTIC_ID || 'UA-135446199-1',
+      },
+    },
+    {
+      use: '@gridsome/plugin-sitemap',
+      options: {
+        cacheTime: 600000, // default
+      },
+    },
+    {
+      use: 'gridsome-plugin-rss',
+      options: {
+        contentTypeName: 'Post',
+        feedOptions: {
+          title: 'gaspadat site',
+          feed_url: 'https://gaspadat.netlify.com/feed.xml',
+          site_url: 'https://gaspadat.netlify.com',
+        },
+        feedItemOptions: node => ({
+          title: node.title,
+          description: node.description,
+          url: 'https://gaspadat.netlify.com' + node.path,
+          author: node.author,
+          date: node.date,
+        }),
+        output: {
+          dir: './static',
+          name: 'feed.xml',
+        },
+      },
+    },
+  ],
+
+  templates: {
+    Post: '/:title',
+    Tag: '/tag/:id',
+    Author: '/author/:id',
+  },
+
+  chainWebpack: config => {
+    config.module
+      .rule('css')
+      .oneOf('normal')
+      .use('postcss-loader')
+      .tap(options => {
+        options.plugins.unshift(...[
+          require('postcss-import'),
+          require('postcss-nested'),
+          require('tailwindcss'),
+        ])
+
+        if (process.env.NODE_ENV === 'production') {
+          options.plugins.push(...[
+            require('@fullhuman/postcss-purgecss')({
+              content: [
+                'src/assets/**/*.css',
+                'src/**/*.vue',
+                'src/**/*.js'
+              ],
+              extractors: [
+                {
+                  extractor: TailwindExtractor,
+                  extensions: ['css', 'vue', 'js']
+                }
+              ],
+              whitelistPatterns: [/shiki/]
+            }),
+          ])
+        }
+
+        return options
+      })
+  },
+}
