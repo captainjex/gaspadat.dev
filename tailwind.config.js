6213790eed40490acc1ebe3043d4b00e199ab3e8
wkwksama <mailpakgasing@gmail.com>
2020-05-16 13:27:38 +0700

    color and default

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,18 @@ module.exports = {
   ],
   theme: {
     fontFamily: {
-      serif: ['"Playfair Display"', 'serif']
+      serif: ['"Playfair Display"', 'serif'],
+      sans: ['"Nunito Sans"', 'sans-serif']
+    },
+    colors: {
+      primary: '#090909',
+      'gray-100': '#f0f0f0',
+      'gray-200': '#b6bdc7',
+      'gray-500': '#969a9f',
+      'gray-700': '#5e5e5e',
+      'gray-800': '#383d45',
+      black: '#000000',
+      white: '#ffffff',
     },
     extend: {},
   },
