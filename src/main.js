59fd9044a35c7caeaa9588e80dbb3f1c39c72444
wkwksama <mailpakgasing@gmail.com>
2020-05-16 15:05:28 +0700

    fix color scheme

--- src/main.js
+++ src/main.js
@@ -8,7 +8,7 @@ export default function(Vue, { head }) {
   Vue.component("Layout", DefaultLayout);
   head.link.push({
     rel: 'stylesheet',
-    href: 'https://fonts.googleapis.com/css2?family=Nunito+Sans:ital,wght@0,400;0,700;1,400;1,700&family=Playfair+Display:wght@400;500;700&display=swap'
+    href: 'https://fonts.googleapis.com/css2?family=Nunito+Sans:ital,wght@0,400;0,600;0,700;1,400;1,600;1,700&family=Playfair+Display:wght@400;500;700&display=swap'
   })
   head.bodyAttrs = {
     class: settings.dark_mode ? "dark" : ""
