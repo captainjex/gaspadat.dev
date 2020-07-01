3ebd8a53ea9b489b6f91b733ee97f14980169bc0
gaspadat <a.sharofuddin@gmail.com>
2020-07-01 22:46:04 +0700

    fix contact and color

--- pages/index.vue
+++ pages/index.vue
@@ -1,13 +1,13 @@
 <template>
-  <div>
-    <section class="flex justify-center items-center min-h-screen text-center italic">
-      <h1 class="text-6xl">
+  <div class="bg-gray-100">
+    <section class="flex justify-center items-center min-h-screen text-center ">
+      <h1 class="text-6xl text-primary italic">
         gaspadat
       </h1>
     </section>
-    <section class="flex justify-center items-center -m-20">
-      <div class="shadow-inner p-6 text-gray-700 bg-gray-100">
-        <div class="text-xl font-medium">
+    <section class="flex justify-center items-center px-2">
+      <div class="shadow-inner bg-gray-200 -mt-8 mb-20 p-6 text-primary text-lg font-light w-full max-w-screen-sm">
+        <div class="text-3xl font-bold">
           Muhammad Arji' Sharofuddin
         </div>
         <div>
@@ -16,23 +16,40 @@
         <div>
           Semarang, Indonesia
         </div>
-        <div class="mt-6 text-right">
-          <div>
-            <a href="mailto:hello@gaspadat.dev" class="underline">Email me</a>
-            hello@gaspadat.dev
+        <div class="mt-12 text-right">
+          <div
+            v-for="(contact, i) in contacts"
+            :key="i"
+            :class="{
+              'mt-2': i > 0,
+              'mb-3': i === 0
+            }"
+          >
+            {{ contact.text }}
+            <a :href="contact.href" class="shadow-md rounded-md px-2 py-1 ml-1 bg-primary text-white" target="_blank" rel="noopener noreferrer">
+              <fa-icon :icon="contact.icon" />
+            </a>
           </div>
         </div>
       </div>
     </section>
-    <div class="max-w-screen-md">
-      <div class="w-full" />
-      <div class="pt-4" />
-    </div>
   </div>
 </template>
 
 <script>
-export default {}
+export default {
+  data () {
+    return {
+      contacts: [
+        { text: 'hello@gaspadat.dev', href: 'mailto:hello@gaspadat.dev', icon: 'envelope' },
+        { text: 'medium.com/@gaspadat', href: 'https://medium.com/@gaspadat', icon: ['fab', 'medium'] },
+        { text: 'github.com/gaspadat', href: 'https://github.com/gaspadat', icon: ['fab', 'github'] },
+        { text: 'twitter.com/gaspadat', href: 'https://twitter.com/gaspadat', icon: ['fab', 'twitter'] },
+        { text: 'instagr.am/gaspadat', href: 'https://instagr.am/gaspadat', icon: ['fab', 'instagram'] }
+      ]
+    }
+  }
+}
 </script>
 
 <style>
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,12 @@ module.exports = {
     './src/**/*.jsx'
   ],
   theme: {
-    extend: {}
+    extend: {
+      colors: {
+        primary: '#2b4851',
+        accent: '#d3d3d3'
+      }
+    }
   },
   variants: {},
   plugins: []
